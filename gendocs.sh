482a98ff152a6233d501b6c84af7a3d3a459497c
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-01-02 17:05:41 +0100

    Update gendocs script

--- gendocs.sh
+++ gendocs.sh
@@ -4,12 +4,28 @@
 out=$1 
 parent="Knowledge Graph"
 
-# create individual docs
-python $SHACL2MD_HOME/shacl2md.py organizations/organizations.rdfs.ttl organizations/organizations.shacl.ttl general/skos.rdfs.ttl  --name organization --out $out --parent "$parent" --nav_order 1 --language nl en --vdir --validate
-python $SHACL2MD_HOME/shacl2md.py objects/objects.rdfs.ttl objects/objects.shacl.ttl general/skos.rdfs.ttl  --name object --out $out --parent "$parent" --nav_order 2 --language nl en --vdir --validate 
-python $SHACL2MD_HOME/shacl2md.py description/description.shacl.ttl  description/description.rdfs.ttl objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl general/skos.rdfs.ttl  --name descriptive --out $out --parent "$parent" --nav_order 3 --language nl en --vdir --validate
-python $SHACL2MD_HOME/shacl2md.py events/events.shacl.ttl  events/events.rdfs.ttl objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl general/skos.rdfs.ttl   --name events --out $out --parent "$parent" --nav_order 4 --language nl en --vdir --validate
-python $SHACL2MD_HOME/shacl2md.py general/skos.shacl.ttl  general/skos.rdfs.ttl general/skos-xl.rdfs.ttl  --name thesauri --out $out/0.0.1 --parent "$parent" --nav_order 5 --language nl en --validate
+declare -a models
+models[0]='organization=organizations/organizations.shacl.ttl,organizations/organizations.rdfs.ttl'
+models[1]='object=objects/objects.shacl.ttl,objects/objects.rdfs.ttl'
+models[2]='descriptive=description/description.shacl.ttl,description/description.rdfs.ttl'
+models[3]='events=events/events.shacl.ttl,events/events.rdfs.ttl'
+models[4]='thesauri=general/skos.shacl.ttl,general/skos.rdfs.ttl,general/skos-xl.rdfs.ttl'
+
+for i in $(seq 0 1 $((${#models[@]}-1)))
+do
+    IFS='=' read -r -a model <<< "${models[$i]}"
+    # select model files
+    IFS=',' read -r -a model_files <<< "${model[@]:1}"
+    # IFS=',' read -r -a model <<< "${models[$i]}"
+    # select all models in models array except the 'i-th' one'
+    crosslinks=$(IFS=' '; echo "${models[@]:0:${i}}" "${models[@]:$(($i+1))}")
+    # echo $crosslinks
+    python $SHACL2MD_HOME/shacl2md.py ${model_files[@]} --name ${model[0]} --out $out --parent "$parent" --nav_order $i --language nl en --crosslinks $crosslinks  --vdir --validate 
+done
+
+# Temp. workaround for missing version
+mv $out/None/thesauri $out/0.0.1
+rm -rf $out/None
 
 # create diagram of all
-python $SHACL2MD_HOME/shacl2md.py events/events.shacl.ttl description/description.shacl.ttl objects/objects.shacl.ttl organizations/organizations.shacl.ttl general/skos.shacl.ttl  events/events.rdfs.ttl objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl description/description.rdfs.ttl general/skos.rdfs.ttl general/skos-xl.rdfs.ttl --name all --out $out --parent "$parent" --nav_order 4 --language nl en --vdir --validate --nodocs
+python $SHACL2MD_HOME/shacl2md.py events/events.shacl.ttl description/description.shacl.ttl objects/objects.shacl.ttl organizations/organizations.shacl.ttl general/skos.shacl.ttl  events/events.rdfs.ttl objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl description/description.rdfs.ttl general/skos.rdfs.ttl general/skos-xl.rdfs.ttl --name all --out $out --parent "$parent" --language nl en --vdir --validate --nodocs
