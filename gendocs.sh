afc8a161c5c806f90e8dc1c84c4a1c05956fdd56
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-01-04 13:32:13 +0100

    Rename thesauri and update gendocs to latest shacl2md

--- gendocs.sh
+++ gendocs.sh
@@ -9,7 +9,7 @@ models[0]='organization=organizations/organizations.shacl.ttl,organizations/orga
 models[1]='object=objects/objects.shacl.ttl,objects/objects.rdfs.ttl'
 models[2]='descriptive=description/description.shacl.ttl,description/description.rdfs.ttl,objects/objects.rdfs.ttl'
 models[3]='events=events/events.shacl.ttl,events/events.rdfs.ttl,objects/objects.rdfs.ttl'
-models[4]='thesauri=general/skos.shacl.ttl,general/skos.rdfs.ttl,general/skos-xl.rdfs.ttl'
+models[4]='terms=general/terms.shacl.ttl,general/skos.rdfs.ttl,general/skos-xl.rdfs.ttl'
 
 for i in $(seq 0 1 $((${#models[@]}-1)))
 do
@@ -20,13 +20,8 @@ do
     # select all models in models array except the 'i-th' one'
     crosslinks=$(IFS=' '; echo "${models[@]:0:${i}}" "${models[@]:$(($i+1))}")
     # echo $crosslinks
-    python $SHACL2MD_HOME/shacl2md.py ${model_files[@]} --name ${model[0]} --out $out --parent "$parent" --nav_order $i --language nl en --crosslinks $crosslinks  --vdir --validate 
+    python $SHACL2MD_HOME/shacl2md.py ${model_files[0]} --ontology ${model_files[@]:1} --name ${model[0]} --out $out --parent "$parent" --nav_order $i --language nl en --crosslinks $crosslinks  --vdir --validate 
 done
 
-# Temp. workaround for missing version
-rm -rf $out/0.0.1/thesauri
-mv $out/None/thesauri $out/0.0.1
-rm -rf $out/None
-
 # create diagram of all
-python $SHACL2MD_HOME/shacl2md.py events/events.shacl.ttl description/description.shacl.ttl objects/objects.shacl.ttl organizations/organizations.shacl.ttl general/skos.shacl.ttl  events/events.rdfs.ttl objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl description/description.rdfs.ttl general/skos.rdfs.ttl general/skos-xl.rdfs.ttl --name all --out $out --parent "$parent" --language nl en --vdir --validate --nodocs
+python $SHACL2MD_HOME/shacl2md.py events/events.shacl.ttl description/description.shacl.ttl objects/objects.shacl.ttl organizations/organizations.shacl.ttl general/terms.shacl.ttl --ontology  events/events.rdfs.ttl objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl description/description.rdfs.ttl general/skos.rdfs.ttl general/skos-xl.rdfs.ttl --name all --out $out --parent "$parent" --language nl en --vdir --validate --nodocs
