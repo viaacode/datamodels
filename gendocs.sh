6691db71c6af27547db7668ae375a37759783083
milan <milanvaladou@protonmail.com>
2023-01-12 13:31:08 +0100

    META-135 Update gendocs script

--- gendocs.sh
+++ gendocs.sh
@@ -5,11 +5,11 @@ out=$1
 parent="Knowledge Graph"
 
 declare -a models
-models[0]='organization=organizations/organizations.shacl.ttl,organizations/organizations.rdfs.ttl'
-models[1]='object=objects/objects.shacl.ttl,objects/objects.rdfs.ttl'
-models[2]='descriptive=description/description.shacl.ttl,description/description.rdfs.ttl,objects/objects.rdfs.ttl'
-models[3]='events=events/events.shacl.ttl,events/events.rdfs.ttl,objects/objects.rdfs.ttl'
-models[4]='terms=general/terms.shacl.ttl,general/skos.rdfs.ttl,general/skos-xl.rdfs.ttl'
+models[0]='organization=organizations/organizations.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
+models[1]='object=objects/objects.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
+models[2]='descriptive=description/description.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
+models[3]='events=events/events.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
+models[4]='terms=terms/terms.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
 
 for i in $(seq 0 1 $((${#models[@]}-1)))
 do
@@ -24,4 +24,4 @@ do
 done
 
 # create diagram of all
-python $SHACL2MD_HOME/shacl2md.py events/events.shacl.ttl description/description.shacl.ttl objects/objects.shacl.ttl organizations/organizations.shacl.ttl general/terms.shacl.ttl --ontology  events/events.rdfs.ttl objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl description/description.rdfs.ttl general/skos.rdfs.ttl general/skos-xl.rdfs.ttl --name all --out $out --parent "$parent" --language nl en --vdir --validate --nodocs
+python $SHACL2MD_HOME/shacl2md.py events/events.ttl description/description.ttl objects/objects.ttl organizations/organizations.ttl terms/terms.ttl --ontology  ontologies/dct.rdfs.ttl ontologies/ebucore.rdfs.ttl ontologies/edm.rdfs.ttl ontologies/foaf.rdfs.ttl ontologies/org.rdfs.ttl ontologies/premis.rdfs.ttl ontologies/prov.rdfs.ttl ontologies/rdf.rdfs.ttl ontologies/schema.rdfs.ttl ontologies/seq.rdfs.ttl ontologies/skos-xl.rdfs.ttl ontologies/skos.rdfs.ttl --name all --out $out --parent "$parent" --language nl en --vdir --validate --nodocs
