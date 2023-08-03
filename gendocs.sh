32f4df4423f4f86db492a3e2fae436199b469a6d
lennertvandevelde <lennert.van.de.velde@outlook.be>
2023-08-03 10:46:38 +0200

    Let gendoc use new shacl2md version

--- gendocs.sh
+++ gendocs.sh
@@ -4,24 +4,14 @@
 out=$1 
 parent="Knowledge Graph"
 
-declare -a models
-models[0]='organization=organizations/organizations.shacl.ttl,objects/objects.rdfs.ttl,organizations/organizations.rdfs.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
-models[1]='object=objects/objects.shacl.ttl,objects/objects.rdfs.ttl,organizations/organizations.rdfs.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
-models[2]='descriptive=description/description.shacl.ttl,objects/objects.rdfs.ttl,organizations/organizations.rdfs.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
-models[3]='events=events/events.shacl.ttl,objects/objects.rdfs.ttl,organizations/organizations.rdfs.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
-models[4]='terms=terms/terms.shacl.ttl,objects/objects.rdfs.ttl,organizations/organizations.rdfs.ttl,ontologies/dct.rdfs.ttl,ontologies/ebucore.rdfs.ttl,ontologies/edm.rdfs.ttl,ontologies/foaf.rdfs.ttl,ontologies/org.rdfs.ttl,ontologies/premis.rdfs.ttl,ontologies/prov.rdfs.ttl,ontologies/rdf.rdfs.ttl,ontologies/schema.rdfs.ttl,ontologies/seq.rdfs.ttl,ontologies/skos-xl.rdfs.ttl,ontologies/skos.rdfs.ttl'
-
-for i in $(seq 0 1 $((${#models[@]}-1)))
-do
-    IFS='=' read -r -a model <<< "${models[$i]}"
-    # select model files
-    IFS=',' read -r -a model_files <<< "${model[@]:1}"
-    # IFS=',' read -r -a model <<< "${models[$i]}"
-    # select all models in models array except the 'i-th' one'
-    crosslinks=$(IFS=' '; echo "${models[@]:0:${i}}" "${models[@]:$(($i+1))}")
-    # echo $crosslinks
-    python $SHACL2MD_HOME/shacl2md.py ${model_files[0]} --ontology ${model_files[@]:1} --name ${model[0]} --out $out --parent "$parent" --nav_order $i --language nl en fr --crosslinks $crosslinks  --vdir --validate 
-done
-
-# create diagram of all
-python $SHACL2MD_HOME/shacl2md.py events/events.shacl.ttl description/description.shacl.ttl objects/objects.shacl.ttl organizations/organizations.shacl.ttl terms/terms.shacl.ttl --ontology  objects/objects.rdfs.ttl organizations/organizations.rdfs.ttl ontologies/dct.rdfs.ttl ontologies/ebucore.rdfs.ttl ontologies/edm.rdfs.ttl ontologies/foaf.rdfs.ttl ontologies/org.rdfs.ttl ontologies/premis.rdfs.ttl ontologies/prov.rdfs.ttl ontologies/rdf.rdfs.ttl ontologies/schema.rdfs.ttl ontologies/seq.rdfs.ttl ontologies/skos-xl.rdfs.ttl ontologies/skos.rdfs.ttl --name all --out $out --parent "$parent" --language nl en fr --vdir --validate --nodocs
+shacl2md \
+    organization:organizations/organizations.shacl.ttl \
+    object:objects/objects.shacl.ttl \
+    descriptive:description/description.shacl.ttl \
+    events:events/events.shacl.ttl \
+    terms:terms/terms.shacl.ttl \
+    --languages en --languages nl --languages fr \
+    --output_dir $out \
+    --ontology_file objects/objects.rdfs.ttl --ontology_file organizations/organizations.rdfs.ttl --ontology_file ontologies/dct.rdfs.ttl --ontology_file ontologies/ebucore.rdfs.ttl --ontology_file ontologies/edm.rdfs.ttl --ontology_file ontologies/foaf.rdfs.ttl --ontology_file ontologies/org.rdfs.ttl --ontology_file ontologies/premis.rdfs.ttl --ontology_file ontologies/prov.rdfs.ttl --ontology_file ontologies/rdf.rdfs.ttl --ontology_file ontologies/schema.rdfs.ttl --ontology_file ontologies/seq.rdfs.ttl --ontology_file ontologies/skos-xl.rdfs.ttl --ontology_file ontologies/skos.rdfs.ttl \
+    --jekyll_parent_page $parent \
+    --crosslink --version_directory --shacl_shacl_validation
